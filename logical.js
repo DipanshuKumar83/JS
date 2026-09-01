7cb4fd1d6dab420cff1ff8c8f5ee97b6865f8427
Dipanshu Kumar <153203348+DipanshuKumar83@users.noreply.github.com>
2026-09-01 21:42:13 +0530

    Add files via upload

---
+++ logical.js
@@ -0,0 +1,10 @@
+let islogin=true;
+let islogout=false;
+let inemail="true";
+
+console.log(islogin && islogout);
+console.log(islogin || islogout);
+console.log(!islogin);
+console.log(!islogout);
+console.log(islogin && inemail);
+console.log(islogin || inemail);
