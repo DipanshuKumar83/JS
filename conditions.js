f2aeaa3f294ef9e9560854290f3e3648a4b175a7
Dipanshu Kumar <153203348+DipanshuKumar83@users.noreply.github.com>
2026-09-03 11:03:55 +0530

    Add files via upload

---
+++ conditions.js
@@ -0,0 +1,19 @@
+let num1=5
+let num2=10
+if(num1>num2){
+    console.log("num1 is greater than num2");
+}
+else{
+    console.log("num2 is greater than num1");
+}
+
+
+let item=["apple","banana","mango"]
+console.log(item.length);
+
+if (item.length===0){
+    console.log("array is empty");
+}
+else{
+    console.log("array is not empty");
+}
