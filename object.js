04735a1664e373ae1c089cf546629244afb75000
Dipanshu Kumar <153203348+DipanshuKumar83@users.noreply.github.com>
2026-09-09 17:20:23 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,10 @@
+let MY_details = {
+    "name": "Dipasnshu",
+    "phone": 1234567890,
+    "Address": "123 Main Street"
+};
+
+console.log(MY_details);
+
+MY_details.email = "dipanshu@gmail.com";
+console.log(MY_details);
