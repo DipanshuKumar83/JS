f08a1cbd7243459d7eb1f357790928b6f07cbdc5
Dipanshu Kumar <153203348+DipanshuKumar83@users.noreply.github.com>
2026-09-11 18:54:33 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,17 @@
+let name="Dipanshu Kr Yadav"
+let friend="Pawan "
+
+console.log(`i am ${name} and my friend_name is ${friend}`)
+
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+console.log(name[3]);
+console.log(name[4]);
+console.log(name[5]);
+console.log(name[6]);
+console.log(name[7]);
+console.log(name[8]);
+
+
+console.log(name.toUpperCase());
